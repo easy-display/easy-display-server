4929ffc8e8d5bbe67ba56b1f5c430e24ac28d66e
Mohammed O. Tillawy <tillawy@gmail.com>
2018-08-20 13:39:12 +0300

    use redis port & redis password

--- dist/index.js
+++ dist/index.js
@@ -50,6 +50,8 @@ const redisClient = redis_1.default.createClient({
     detect_buffers: true,
     host: process.env.REDIS_HOST,
     port: +process.env.REDIS_PORT,
+    password: process.env.REDIS_PASS,
+    db: process.env.REDIS_DB
 });
 const currentSockets = {};
 exports.staticFiles = (req, res) => {
--- src/index.ts
+++ src/index.ts
@@ -71,6 +71,8 @@ const redisClient = redis.createClient({
     detect_buffers: true,
     host: process.env.REDIS_HOST,
     port: +process.env.REDIS_PORT,
+    password: process.env.REDIS_PASS,
+    db: process.env.REDIS_DB
 });
 
 interface ICurrentSocketType {
